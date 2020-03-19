5eef3fc5e799cd3abb0ae744b62bb002ee63195e
Yash Jain <31776391+YashJainHSR@users.noreply.github.com>
2020-03-19 16:39:48 +0530

    API Authentication and Security

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -49,6 +50,12 @@ const userSchema = new mongoose.Schema({
     }]
 });
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+});
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this;
     const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse', { expiresIn: '7 days' });
@@ -90,7 +97,13 @@ userSchema.pre('save', async function (next) {
     }
 
     next();
-})
+});
+
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({ owner: user._id });
+    next();
+});
 
 const User = mongoose.model('User', userSchema);
 
